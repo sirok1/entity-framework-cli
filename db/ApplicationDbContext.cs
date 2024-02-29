93eaf40dda430b04de6a882afdc274c8895dc9a6
sirok1 <s1r0k.dev@gmail.com>
2024-03-01 00:20:44 +0300

    feat(main): db context

---
+++ db/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using working_with_db.db.entities;
+
+namespace working_with_db.db;
+
+public class ApplicationDbContext : DbContext
+{
+    private readonly IConfiguration _config;
+
+    public DbSet<TypeDb> Types { get; set; } = null!;
+
+    public ApplicationDbContext(IConfiguration config)
+    {
+        _config = config;
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql($"Host={_config["DB_HOST"]};Port={_config["DB_PORT"]};Database={_config["POSTGRES_DB"]};Username={_config["POSTGRES_USER"]};Password={_config["POSTGRES_PASSWORD"]}");
+    }
+}
