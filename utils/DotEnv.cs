72487d89371b70f07bb034fdc3a3dfb7ef186cba
sirok1 <s1r0k.dev@gmail.com>
2024-03-01 00:20:32 +0300

    feat(main): using .env

---
+++ utils/DotEnv.cs
@@ -0,0 +1,19 @@
+﻿namespace working_with_db;
+
+public class DotEnv
+{
+    public static void Load(string filepath)
+    {
+        if (!File.Exists(filepath))
+            return;
+        foreach (var line in File.ReadAllLines(filepath))
+        {
+            var parts = line.Split(
+                '=',
+                StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length != 2)
+                continue;
+            Environment.SetEnvironmentVariable(parts[0], parts[1]);
+        }
+    }
+}
