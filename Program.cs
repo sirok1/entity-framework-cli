e3cca44b0b56a075c4130e09bf00019105e240e5
sirok1 <s1r0k.dev@gmail.com>
2024-03-01 11:47:26 +0300

    feat(main): bd connect with env variables

--- Program.cs
+++ Program.cs
@@ -1,17 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using working_with_db;
+﻿using working_with_db;
 
-namespace working_with_db
-{
-    class Program
-    {
-        public static void Main(string[] args)
-            => CreateHostBuilder().Build().Run();
-        
-        public static IHostBuilder CreateHostBuilder(string[] args)
-            => Host.CreateDefaultBuilder(args)
-    }
-}
-
-
+var root = Directory.GetCurrentDirectory();
+var dotenv = Path.Combine(root, ".env");
+DotEnv.Load(dotenv);
--- db/ApplicationDbContext.cs
+++ db/ApplicationDbContext.cs
@@ -8,8 +8,16 @@ public class ApplicationDbContext : DbContext
 {
     public DbSet<TypeDb> Types { get; set; } = null!;
 
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
+    public ApplicationDbContext()
     {
+        var root = Directory.GetCurrentDirectory();
+        var dotenv = Path.Combine(root, ".env");
+        DotEnv.Load(dotenv);
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql( $"Host={Environment.GetEnvironmentVariable("DB_HOST")};Port={Environment.GetEnvironmentVariable("DB_PORT")};Database={Environment.GetEnvironmentVariable("POSTGRES_DB")};Username={Environment.GetEnvironmentVariable("POSTGRES_USER")};Password={Environment.GetEnvironmentVariable("POSTGRES_PASSWORD")}");
     }
 }
