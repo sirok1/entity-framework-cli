14c05bf22facd334dbb5ceb04d064ed97f1ff15f
sirok1 <s1r0k.dev@gmail.com>
2024-03-02 04:03:13 +0300

    feat(main): menu model

---
+++ Json/MenuSettings/MenuModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Entity_framework_cli.Json.MenuSettings;
+
+public class MenuModel
+{
+    public List<string> types { get; set; } = null!;
+    public Dictionary<string, Options> options { get; set; } = null!;
+    public string endPrompt { get; set; } = null!;
+    public string unknownChoicePrompt { get; set; } = null!;
+}
---
+++ Json/MenuSettings/Options.cs
@@ -0,0 +1,7 @@
+﻿namespace Entity_framework_cli.Json.MenuSettings;
+
+public class Options
+{
+    public string title { get; set; } = null!;
+    public List<string> items { get; set; } = null!;
+}
